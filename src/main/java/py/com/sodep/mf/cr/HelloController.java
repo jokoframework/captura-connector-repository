f709e281fb8483676141b9d41b400089564f6d6e
Matías Irala <44922906+Dyuko@users.noreply.github.com>
2020-03-16 15:32:00 -0300

    Implementa el proyecto Spring Boot inicial

---
+++ src/main/java/py/com/sodep/mf/cr/HelloController.java
@@ -0,0 +1,14 @@
+package py.com.sodep.mf.cr;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
